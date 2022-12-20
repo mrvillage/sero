d44683dda8b76b34f0073c26eadcd7f0908e3854
Village <mr.village05@gmail.com>
2022-12-20 12:15:06 -0500

    Add crate description for docs.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+/// Sero is a simple and lightweight library for maintaining a shared store of locks.
+///
+/// ## Basic Usage
+///
+/// ```rust
+/// use sero::LockStore;
+///
+/// let store = LockStore::new();
+///
+/// // to lock asynchronously use
+/// let guard = store.lock("key").await;
+/// // to lock synchronously use
+/// let guard = store.lock("key").wait();
+/// // NOTE: synchronously locking will "park" the current thread until the lock is acquired
+///
+/// // locks are released when the LockGuard is dropped
+/// // either with the drop function or when they go out of scope
+/// drop(guard);
+/// ```
 use std::{
     collections::VecDeque,
     future::Future,
